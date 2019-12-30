7db45bdbdf39c7d0eaae8fecd87cb28d3e3c279a
poklipart <angellopezandrade4@gmail.com>
2019-12-30 17:08:37 +1100

    group search complete excepting multiple condition db querying

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,16 @@
 class GroupsController < ApplicationController
+    before_action :permit_search_params
+
     def index
-        @groups = Group.all
+        if !@search_params[:duration] and !@search_params[:workout_focus] and !@search_params[:gender]
+            @groups = Group.all
+        else
+            @groups = Group.where(duration: @search_params[:duration], workout_focus: @search_params[:workout_focus], gender: @search_params[:gender].downcase)
+        end
     end
 
-    def search
+    private
+    def permit_search_params
+        @search_params = params.fetch(:form, {}).permit(:workout_focus, :duration, :gender)
     end
 end
