adc28e18a94b6a83797817aeb7d8d40c0fcf4195
poklipart <angellopezandrade4@gmail.com>
2019-12-30 17:36:14 +1100

    search tinkering

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,9 +2,10 @@ class GroupsController < ApplicationController
     before_action :permit_search_params
 
     def index
-        if !@search_params[:duration] and !@search_params[:workout_focus] and !@search_params[:gender]
+        if !params[:form]
             @groups = Group.all
         else
+            #how to only query db fields that aren't nil in the form params?
             @groups = Group.where(duration: @search_params[:duration], workout_focus: @search_params[:workout_focus], gender: @search_params[:gender].downcase)
         end
     end
