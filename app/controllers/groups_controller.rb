092afbf65715972668d869b1dc8d666819452227
poklipart <angellopezandrade4@gmail.com>
2019-12-31 18:34:54 +1100

    group search functionality complete (groups index and homepage views)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,11 +2,24 @@ class GroupsController < ApplicationController
     before_action :permit_search_params
 
     def index
+        #better way to combine activerecord relations? - 31/12
         if !params[:form]
             @groups = Group.all
         else
-            #how to only query db fields that aren't nil in the form params?
-            @groups = Group.where(duration: @search_params[:duration], workout_focus: @search_params[:workout_focus], gender: @search_params[:gender].downcase)
+            all_matches = Group.none
+            if @search_params[:duration]
+                duration_matches = Group.where(duration: @search_params[:duration])
+                all_matches = all_matches.or duration_matches
+            end
+            if @search_params[:workout_focus]
+                workout_focus_matches = Group.where(workout_focus: @search_params[:workout_focus])
+                all_matches = all_matches.or workout_focus_matches
+            end
+            if @search_params[:gender]
+                gender_matches = Group.where(gender: @search_params[:gender].downcase)
+                all_matches = all_matches.or gender_matches
+            end
+            @groups = all_matches
         end
     end
 
