6c694ce8a2be0118ec0847eb5250e7001e9b7277
poklipart <angellopezandrade4@gmail.com>
2020-01-03 16:20:43 +1100

    fixed group search functionality so that all conditions are considered rather than only one

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,24 +2,22 @@ class GroupsController < ApplicationController
     before_action :permit_search_params
 
     def index
-        #better way to combine activerecord relations? - 31/12
-        #need to use merge instead of or? can't test intersection with current data set - 31/12
         if !params[:form]
             @groups = Group.all
         else
-            all_matches = Group.none
+            all_matches = Group.all
             if @search_params[:duration]
                 duration_matches = Group.where(duration: @search_params[:duration])
-                all_matches = all_matches.or duration_matches
             end
             if @search_params[:workout_focus]
                 workout_focus_matches = Group.where(workout_focus: @search_params[:workout_focus])
-                all_matches = all_matches.or workout_focus_matches
             end
             if @search_params[:gender]
                 gender_matches = Group.where(gender: @search_params[:gender].downcase)
-                all_matches = all_matches.or gender_matches
             end
+            all_matches = all_matches.merge duration_matches if duration_matches
+            all_matches = all_matches.merge workout_focus_matches if workout_focus_matches
+            all_matches = all_matches.merge gender_matches if gender_matches
             @groups = all_matches
         end
     end
